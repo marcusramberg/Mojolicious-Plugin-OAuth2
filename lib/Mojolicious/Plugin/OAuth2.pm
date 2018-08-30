7b2da19a1bea276a45b6cb847cdde51bd60c39ae
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-08-30 12:28:17 +0200

    Fix promise based synopsis

--- lib/Mojolicious/Plugin/OAuth2.pm
+++ lib/Mojolicious/Plugin/OAuth2.pm
@@ -240,8 +240,8 @@ L<IO::Socket::SSL> is installed.
     my $get_token_args = {redirect_uri => $c->url_for("connect")->userinfo(undef)->to_abs};
 
     $c->oauth2->get_token_p(facebook => $get_token_args)->then(sub {
-      my $data = shift;
-      $c->session(token => $data->{access_token});
+      return unless my $provider_res = shift; # Redirct to Facebook
+      $c->session(token => $provider_res->{access_token});
       $c->redirect_to("profile");
     })->catch(sub {
       $c->render("connect", error => shift);
